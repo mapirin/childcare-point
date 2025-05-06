0dc6cdd6ccf727377ed79fac508f03b8d35297f6
mapirin <812matahiro29@gmail.com>
2025-05-06 15:47:17 +0900

    webhook検証 message1-5

--- childcare-point/src/main/java/com/childcare/point/service/linebot/LineBotService.java
+++ childcare-point/src/main/java/com/childcare/point/service/linebot/LineBotService.java
@@ -1,18 +1,17 @@
 package com.childcare.point.service.linebot;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Map;
 
-import com.linecorp.bot.client.LineMessagingClient;
-import com.linecorp.bot.model.PushMessage;
-import com.linecorp.bot.model.message.TextMessage;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class LineBotService {
-	
-	@Autowired
-	private LineMessagingClient lineMessagingClient;
-	
+
 	/**
 	 * LINE用APIで使用するメッセージ送信処理
 	 * 
@@ -20,9 +19,19 @@ public class LineBotService {
 	 * @param message
 	 */
 	public void sendMessage(String userId, String message) {
-		TextMessage textMessage = new TextMessage(message);
-		PushMessage pushMessage = new PushMessage(userId, textMessage);
-		lineMessagingClient.pushMessage(pushMessage);
-	}
 
+		RestTemplate restTemplate = new RestTemplate();
+		HttpHeaders httpHeaders = new HttpHeaders();
+		httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+		httpHeaders.set("Autorization", "Bearer" + System.getenv("CHANNEL_ACCESS_TOKEN"));
+
+		Map<String, Object> requestBody = Map.of(
+				"to", userId,
+				"messages", List.of(Map.of(
+						"type", "text",
+						"text", message)));
+
+		HttpEntity<Map<String, Object>> request = new HttpEntity<>(requestBody, httpHeaders);
+		restTemplate.postForEntity("https://api.line.me/v2/bot/message/push", request, String.class);
+	}
 }
