0509724a1c493108f9ebc06df0f183c6863ede78
mapirin <812matahiro29@gmail.com>
2025-05-31 21:12:55 +0900

    redis対応

--- childcare-point/src/main/java/com/childcare/point/config/RedisConfig.java
+++ childcare-point/src/main/java/com/childcare/point/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package com.childcare.point.config;
 
+import org.springframework.boot.autoconfigure.data.redis.LettuceClientConfigurationBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -8,15 +9,30 @@ import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSeriali
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
+import io.lettuce.core.RedisClient;
+import io.lettuce.core.RedisURI;
+import io.lettuce.core.api.StatefulRedisConnection;
+
 @Configuration
 @EnableRedisHttpSession
 public class RedisConfig {
-//
-//	@Bean
-//    public RedisConnectionFactory redisConnectionFactory() {
-//        String redisUrl = System.getenv("REDIS_URL"); // 環境変数から取得
-//        return new LettuceConnectionFactory(redisUrl);
-//    }
+
+	@Bean
+	LettuceClientConfigurationBuilderCustomizer lettuceClientConfigurationBuilderCustomizer() {
+		return builder -> {
+			if (builder.build().isUseSsl()) {
+				builder.useSsl().disablePeerVerification();
+			}
+		};
+	}
+	
+	public static StatefulRedisConnection<String, String> connect() {
+	    RedisURI redisURI = RedisURI.create(System.getenv("REDIS_URL"));
+	    redisURI.setVerifyPeer(false);
+
+	    RedisClient redisClient = RedisClient.create(redisURI);
+	    return redisClient.connect();
+	}
 
 	@Bean
 	RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
